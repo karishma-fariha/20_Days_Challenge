180be82bf97fef6406c296efa4f0099ebb17abf3
karishma-fariha <karishmafarihakathi10@gmail.com>
2026-06-10 16:38:20 +0600

    added basic problem 14

---
+++ pb13.ts
@@ -0,0 +1,9 @@
+// Problem 13: Remove Duplicates from Array  [Easy]
+// Description: Write a function removeDuplicates(arr) that returns a new array with duplicate values removed.
+// Example:
+// Input: [1, 2, 2, 3, 3, 4]  → Output: [1, 2, 3, 4]
+// Hint: Use Set or filter() with indexOf().
+
+function removeDuplicates(arr:number[]):number[]{
+    return arr.filter((item,index))
+}
