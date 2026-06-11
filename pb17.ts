47047698d3970c996fa3974ec4540a5c0ea91e7e
karishma-fariha <karishmafarihakathi10@gmail.com>
2026-06-11 15:59:33 +0600

    added basic problem 17

---
+++ pb17.ts
@@ -0,0 +1,12 @@
+// Problem 17: Merge Two Objects  [Easy]
+// Description: Write a function mergeObjects(obj1, obj2) that merges two objects into one. If keys conflict, the second object's values win.
+// Example:
+// Input: {a:1}, {b:2}  → Output: {a:1, b:2}
+// Hint: Use the spread operator or Object.assign().
+function mergeObjects<T extends object>(obj1:T, obj2:T):T{
+return {...obj1,...obj2};
+}
+const objA={a:1,c:3};
+const objB = {b:2};
+const merged = mergeObjects(objA,objB);
+console.log(mergeObjects(objA,objB));
