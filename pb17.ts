bacdbe640939cb1fe721b64a421ea9faa99bbab9
karishma-fariha <karishmafarihakathi10@gmail.com>
2026-06-12 23:26:44 +0600

    modify the solution of problem 17

--- pb17.ts
+++ pb17.ts
@@ -3,7 +3,7 @@
 // Example:
 // Input: {a:1}, {b:2}  → Output: {a:1, b:2}
 // Hint: Use the spread operator or Object.assign().
-function mergeObjects<T extends object>(obj1:T, obj2:T):T{
+function mergeObjects<T, U extends object>(obj1:T, obj2:U):T & U{
 return {...obj1,...obj2};
 }
 const objA={a:1,c:3};
