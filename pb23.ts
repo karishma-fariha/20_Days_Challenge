32fba8f1bd0e20ec13c58dc89b8288fdbd67f4f1
karishma-fariha <karishmafarihakathi10@gmail.com>
2026-06-14 15:58:15 +0600

    solved destructure and rename Problem

---
+++ pb23.ts
@@ -0,0 +1,8 @@
+// Problem 23: Create a Counter with Closure  [Medium]
+// Description: Write a function makeCounter() that returns an object with increment, decrement, and getCount methods using closure.
+// Example:
+// const c = makeCounter();c.increment(); c.increment();c.getCount(); // 2
+// Hint: Use a variable inside the outer function that inner functions can access.
+function makeCounter(){
+    return 
+}
